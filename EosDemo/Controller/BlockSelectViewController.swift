8f1e8fd8324dd30f251da2f65ce36edd83d17e64
Ryan Hieber <floatingpoint@gmail.com>
2018-07-17 13:28:52 -0700

    -Added error message and loading indicator to BlockSelectViewController

--- EosDemo/Controller/BlockSelectViewController.swift
+++ EosDemo/Controller/BlockSelectViewController.swift
@@ -10,27 +10,46 @@ import UIKit
 
 class BlockSelectViewController: UIViewController {
 
+    @IBOutlet weak var statusView: UIView!
+    @IBOutlet weak var statusLabel: UILabel!
+
+    @IBAction func onCancelButton(_ sender: Any) {
+        resetStatusView()
+    }
+    
+    @IBOutlet weak var connectionIndicator: UIActivityIndicatorView!
+    
     var currentChainInfoRequest: ChainInfoRequest? = nil
     var currentBlockInfoRequest: BlockInfoRequest? = nil
 
     @IBAction func OnShowHeadBlockButton(_ sender: UIButton) {
         currentChainInfoRequest = ChainInfoRequest(url: URL(string: "https://api.eosnewyork.io/v1/chain/get_info")!)
+        
+        statusView.isHidden = false
+        statusLabel.text = "Connecting..."
+        connectionIndicator.startAnimating()
         currentChainInfoRequest?.load(withCompletion: {[weak self] info in
+            self?.connectionIndicator.stopAnimating()
             guard let chainInfo = info else {
                 // Error loading the chain info.
-                // TODO: Post an error message.
+                self?.statusLabel.text = "Error retrieving chain information"
                 return
             }
             self?.currentBlockInfoRequest = BlockInfoRequest(url: URL(string: "https://api.eosnewyork.io/v1/chain/get_block")!, blockId: chainInfo.headBlockId)
             self?.currentChainInfoRequest = nil
             
+            self?.statusLabel.text = "Retriving Block..."
+            self?.connectionIndicator.startAnimating()
             self?.currentBlockInfoRequest?.load(withCompletion: {[weak self] info in
+                self?.connectionIndicator.stopAnimating()
                 self?.blockInfoToShow = info
                 if info == nil {
                     // Error loading the block.
-                    // TODO: Post an error message.
+                    self?.statusLabel.text = "Error retrieving block information."
                     return
                 }
+                self?.resetStatusView()
+                
                 self?.performSegue(withIdentifier: "ShowBlockInfoSegue", sender: sender)
                 self?.currentBlockInfoRequest = nil
             })
@@ -42,9 +61,16 @@ class BlockSelectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        resetStatusView()
+        
     }
 
+    func resetStatusView() {
+        statusLabel.text = ""
+        connectionIndicator.stopAnimating()
+        statusView.isHidden = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
